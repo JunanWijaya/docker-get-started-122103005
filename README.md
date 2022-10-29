f3162be6ddfa914d99f5d4a6552a84c55f9063cd
JunanWijaya <juned.wijaya@students.paramadina.ac.id>
2022-10-29 11:25:22 +0700

    Modified Readme.md dan app.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # docker-get-started-122103005
 Test Docket Git Sync
 Commit 1 - copas Folder Spec,src, Dockerfile, package.json, yarn.lock
-
+Commit 2 - Change app.js
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above Jun!</p>
+                <p className="text-center">No items yet! Add one above Juned Agung Wijaya!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
