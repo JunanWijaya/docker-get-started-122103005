7dd3b609b2bf60487b489bce2a8b35b5633d6c11
JunanWijaya <juned.wijaya@students.paramadina.ac.id>
2022-10-29 11:27:24 +0700

    Modified Readme.md dan app.js

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Test Docket Git Sync
 Commit 1 - copas Folder Spec,src, Dockerfile, package.json, yarn.lock
 Commit 2 - Change app.js
+Commit 3 - Tambah NIK 122103005
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above Juned Agung Wijaya!</p>
+                <p className="text-center">No items yet! Add one above Juned Agung Wijaya 122103005!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
